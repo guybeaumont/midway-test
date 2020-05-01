5f9000b1c71b5db12efc2c0d13085e166052a762
Guy Beaumont <guybeaumont@gmail.com>
2020-05-01 20:39:01 +0200

    initial commit

--- web/src/layouts/index.tsx
+++ web/src/layouts/index.tsx
@@ -1,43 +1,50 @@
-import React from 'react'
-import Helmet from 'react-helmet'
+import React from "react";
+import Helmet from "react-helmet";
 
-import { Header } from 'src/components/header'
-import { Footer } from 'src/components/footer'
-import { SwitchTransition, Transition } from 'react-transition-group'
-import { Disclaimer } from 'src/components/disclaimer'
-import { CartDrawer } from 'src/components/cartDrawer'
-import { PasswordWrapper } from './password'
+import { Header } from "src/components/header";
+import { Footer } from "src/components/footer";
+import { SwitchTransition, Transition } from "react-transition-group";
+import { Disclaimer } from "src/components/disclaimer";
+import { CartDrawer } from "src/components/cartDrawer";
+import { PasswordWrapper } from "./password";
 
 const TRANSITION_DURATION = 400;
 const TRANSITION_STYLES = {
-	default: {
-		transition: `opacity ${TRANSITION_DURATION}ms ease-in-out`,
-	},
-	entering: {
-		opacity: 0,
-	},
-	entered: {
-		opacity: 1,
-	},
-	exiting: {
-		opacity: 0,
-	},
-	exited: {
-		opacity: 0,
-	},
+  default: {
+    transition: `transform ${TRANSITION_DURATION}ms ease-in-out`
+  },
+  entering: {
+    // opacity: 0,
+    transform: "translateY(100vh)"
+  },
+  entered: {
+    // opacity: 1,
+    transform: "translateY(0vh)"
+  },
+  exiting: {
+    // opacity: 0,
+    transform: "translateY(-100vh)"
+  },
+  exited: {
+    // opacity: 0,
+    transform: "translateY(-100vh)"
+  }
 };
 
-import 'src/styles/main.css'
+import "src/styles/main.css";
 
 const Layout = ({ children, siteMetadata, location }: { children: any }) => {
   return (
     <React.Fragment>
-      <Helmet title='Midway'>
-        <link href='https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap' rel='stylesheet' />
+      <Helmet title="Midway">
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </Helmet>
       <PasswordWrapper>
         <div>
-          <Disclaimer />
+          {/* <Disclaimer /> */}
           <Header />
           <CartDrawer />
           {/* 
@@ -49,20 +56,22 @@ const Layout = ({ children, siteMetadata, location }: { children: any }) => {
             <SwitchTransition>
               <Transition
                 key={location.pathname}
-                mountOnEnter
-                unmountOnExit
-                appear
-                timeout={TRANSITION_DURATION}>
-                  {status => (
-                    <div
-                      style={{
-                        ...TRANSITION_STYLES.default,
-                        ...TRANSITION_STYLES[status],
-                      }}>
-                      {children}
-                      <Footer />
-                    </div>
-                  )}
+                mountOnEnter={true}
+                unmountOnExit={true}
+                appear={true}
+                timeout={TRANSITION_DURATION}
+              >
+                {status => (
+                  <div
+                    style={{
+                      ...TRANSITION_STYLES.default,
+                      ...TRANSITION_STYLES[status]
+                    }}
+                  >
+                    {children}
+                    <Footer />
+                  </div>
+                )}
               </Transition>
             </SwitchTransition>
           ) : (
@@ -74,7 +83,7 @@ const Layout = ({ children, siteMetadata, location }: { children: any }) => {
         </div>
       </PasswordWrapper>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
